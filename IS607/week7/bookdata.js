93d72022dc31f44aaaaec61edd31912643e7e3bc
talham <talha.noorm@gmail.com>
2016-10-17 00:29:20 -0400

    Add files via upload

---
+++ IS607/week7/bookdata.js
@@ -0,0 +1,8 @@
+{
+"ID":["1","2","3","4"],
+"Author":["Greg Mortenson","David Oliver Relin","Jonathan Frenzen","Michael Chabon"],
+"Title":["Three Cups of Tea","Three Cups of Tea","The Corrections","Yiddish Policeman's Union"],
+"Type":["Non-Fiction","Non-Fiction","Fiction","Fiction"],
+"Publisher":["Penguin","Penguin","Picador","Harper Perenial"],
+"Language":["English","English","English","English"]
+}
